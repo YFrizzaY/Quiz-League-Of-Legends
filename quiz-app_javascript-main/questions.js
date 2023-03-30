21dfaa70a3f1cc18d42719e8cce233773519a293
Paulo Henrique <104027031+YFrizzaY@users.noreply.github.com>
2023-03-30 16:34:50 -0300

    Add files via upload

---
+++ quiz-app_javascript-main/questions.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    question: "Sim, aquilo era uma banana",
+    answers: [
+      { option: "Soraka", correct: true },
+      { option: "Jayce", correct: false },
+      { option: "Lee Sin", correct: false },
+    ],
+  },
+  {
+    question: "Lima-Oscar-Lima!",
+    answers: [
+      { option: "Rammus", correct: false },
+      { option: "Corki", correct: true },
+      { option: "Ekko", correct: false },
+    ],
+  },
+  {
+    question: "Ok",
+    answers: [
+      { option: "Nasus", correct: false },
+      { option: "Aatrox", correct: false },
+      { option: "Rammus", correct: true },
+    ],
+  },
+  {
+    question: "Mil vidas darei por Freljord",
+    answers: [
+      { option: "Syndra", correct: false },
+      { option: "Tryndamere", correct: false },
+      { option: "Anivia", correct: true },
+    ],
+  },
+  {
+    question: "Que sejam garras ou lanças, seu fim será o mesmo",
+    answers: [
+      { option: "Xin Zhao", correct: false },
+      { option: "Pantheon", correct: false },
+      { option: "Nidalee", correct: true },
+    ],
+  },
+  {
+    question: "Suas lágrimas salgarão o oceano",
+    answers: [
+      { option: "Gangplank", correct: true },
+      { option: "Pyke", correct: false },
+      { option: "Nautilus", correct: false },
+    ],
+  },
+  {
+    question: "Tenho o melhor trabalho do mundo",
+    answers: [
+      { option: "Jayce", correct: false },
+      { option: "Draven", correct: true },
+      { option: "Garen", correct: false },
+    ],
+  },
+];
